0c0b27dc29491a82f7df4db9634477d691f5aba0
Carlos Adriano <34164119+CarlosAdrianoJp@users.noreply.github.com>
2023-04-05 04:59:17 -0300

    angular inicio portaria liv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { UsuariosModule } from './usuarios/usuarios.module'
+import { TemplateModule } from './template/template.module';
+import { HomeComponent } from './home/home.component'
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UsuariosModule,
+    TemplateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
